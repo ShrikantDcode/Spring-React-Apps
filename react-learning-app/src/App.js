a96c091db5e71133d2196f19dabe3ebd4e6e98f7
shrikantdande <shriiikant@gmail.com>
2024-11-17 21:32:22 +0530

    userList

--- react-learning-app/src/App.js
+++ react-learning-app/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.css";
 import BasicFeatures from "./components/es6-features/BasicFeatures";
-import HomePage from "./components/HomePage";
+//import HomePage from "./components/HomePage";
 import VariableDeclaration from "./components/VariableDeclaration";
 import { Routes, Route } from "react-router-dom";
 import UserList from "./components/UserList";
@@ -10,8 +10,8 @@ import Counter from "./components/Counter";
 function App() {
   return (
     <div>
-      <HomePage />
-      
+      {/* <HomePage /> */}
+      <UserList />
       {/* <Routes>
         <Route path={"/"} element={<HomePage />} exact />
         <Route path={"/user-list"} element={<UserList />} exact />
